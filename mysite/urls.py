f36bc71e1d610ab8c9ecbc6af7a63b3283beb8ba
Tharunthorn Musik <tharunthornmusik1446@gmail.com>
2023-09-10 17:21:40 +0700

    improve css

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/polls/')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
